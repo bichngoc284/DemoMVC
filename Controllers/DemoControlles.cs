5662e5be57e1aaef4db94f283debd83e3ec8f26a
bichngoc284 <Tranthingoc28042004@gmail.com>
2026-06-19 02:36:49 +0700

    BaiThucHanhSo1

---
+++ Controllers/DemoControlles.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+namespace DemoMVC.Controllers;
+public class DemoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
